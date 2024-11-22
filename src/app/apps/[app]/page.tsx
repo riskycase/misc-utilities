0c167b2267a7a3c9574b84632e2d65ec9aa46e40
Hrishikesh Patil <hrishikeshpatil.754@gmail.com>
2024-11-22 11:49:26 +0530

    Minor SEO optimisations

--- src/app/apps/[app]/page.tsx
+++ src/app/apps/[app]/page.tsx
@@ -5,6 +5,7 @@ import {
     BreadcrumbLink,
     Flex,
 } from "@chakra-ui/react";
+import { Metadata } from "next";
 import NextLink from "next/link";
 
 export default function AppUI({ params }: { params: { app: string } }) {
@@ -32,6 +33,14 @@ export default function AppUI({ params }: { params: { app: string } }) {
             <app.element />
         </Flex>
     );
+}
+
+export async function generateMetadata({ params }: { params: { app: string } }): Promise<Metadata> {
+    const app = apps.find((app) => app.identifier === params.app)!!;
+    return Promise.resolve({
+        title: app.displayName,
+        description: app.description
+    })
 }
 
 export async function generateStaticParams() {
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,21 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Providers } from "./providers";
+import { theme } from "@chakra-ui/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Miscallaneous utilities",
+  title: {
+    template: "Miscallaneous utilities | %s",
+    default: "Miscallaneous utilities",
+  },
   description: "Collection of useful snippets for everyday developer",
+};
+
+export const viewport: Viewport = {
+  themeColor: theme.colors.gray[900],
 };
 
 export default function RootLayout({
