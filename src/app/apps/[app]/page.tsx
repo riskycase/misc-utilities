73c93a70570db6f8019dcd644ea3a2a87244a99d
Hrishikesh Patil <hrishikeshpatil.754@gmail.com>
2025-03-31 19:55:39 +0530

    Add credits to footer

--- src/app/apps/[app]/page.tsx
+++ src/app/apps/[app]/page.tsx
@@ -2,13 +2,26 @@ import { apps } from "@/data/apps";
 import { Metadata } from "next";
 import ClientHolder from "./clientHolder";
 import { Suspense } from "react";
+import Footer from "@/app/footer";
+import { Flex } from "@chakra-ui/react";
 
 export default function AppUI({ params }: { params: { app: string } }) {
   const app = apps.find((app) => app.identifier === params.app)!!;
   return (
+    <Flex
+      className="h-full w-full"
+      alignItems="stretch"
+      direction="column"
+      padding={{ base: 4, lg: 8 }}
+      flex={1}
+      justifyContent="start"
+    >
+
     <Suspense>
       <ClientHolder app={app} />
     </Suspense>
+    <Footer />
+    </Flex>
   );
 }
 
